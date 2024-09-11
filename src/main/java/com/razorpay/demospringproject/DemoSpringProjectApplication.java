7904a3fca1eefc68bd1aed866cb48b79e05ba96a
Sarabjeet Singh <sarabjeet.singh@razorpay.com>
2024-09-11 10:23:45 +0530

    initial commit

---
+++ src/main/java/com/razorpay/demospringproject/DemoSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.razorpay.demospringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringProjectApplication.class, args);
+    }
+
+}
